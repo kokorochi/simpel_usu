91a85d73a953fddb2ee0005096172b41c418cea6
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-01-04 14:12:59 +0700

    update .gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# lppm_usu
--- resources/views/layouts/_footer-admin.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Start footer content -->
-<footer class="footer-content clearfix">
-    2016 - <span id="copyright-year"></span> &copy; LPM USU - Pengabdian Masyarakat
-    {{--<span class="pull-right">0.01 GB(0%) of 15 GB used</span>--}}
-</footer><!-- /.footer-content -->
-<!--/ End footer content -->
