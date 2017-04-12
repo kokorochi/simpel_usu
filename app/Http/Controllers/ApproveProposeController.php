944b4894c986872e99a6a4ebd8b86f84a15a3957
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-04-12 11:23:08 +0700

    Add additional information to download excel proposes

--- app/Http/Controllers/ApproveProposeController.php
+++ app/Http/Controllers/ApproveProposeController.php
@@ -338,9 +338,12 @@ class ApproveProposeController extends BlankonController {
                 $data[$i]['Anggota ' . $j++] = "";
             }
             $j = 1;
+            $avg_score = 0;
+            $avg_amount = 0;
             foreach ($research_reviewers as $research_reviewer)
             {
                 $member_info = Lecturer::where('employee_card_serial_number', $research_reviewer->nidn)->first();
+                $data[$i]['Reviewer ' . $j . ' Nama'] = $member_info->full_name;
                 $review_propose = $propose->reviewPropose()->where('nidn', $research_reviewer->nidn)->first();
                 if (! is_null($review_propose))
                 {
@@ -350,18 +353,32 @@ class ApproveProposeController extends BlankonController {
                     {
                         $total_score = $total_score + ($review_propose_i->quality * $review_propose_i->score);
                     }
-                    $member_info->full_name = $member_info->full_name . ', status = ' . $review_propose->status . ', total score = ' . $total_score;
+                    $data[$i]['Reviewer ' . $j . ' Status'] = $review_propose->status;
+                    $data[$i]['Reviewer ' . $j . ' Rekomendasi Dana'] = $review_propose->recommended_amount;
+                    $data[$i]['Reviewer ' . $j . ' Nilai'] = $total_score;
+                    $avg_score = $avg_score + $total_score;
+                    $avg_amount = $avg_amount + $review_propose->recommended_amount;
                 } else
                 {
-                    $member_info->full_name = $member_info->full_name . ', status = belum review';
+                    $data[$i]['Reviewer ' . $j . ' Status'] = 'Belum Review';
+                    $data[$i]['Reviewer ' . $j . ' Rekomendasi Dana'] = '0';
+                    $data[$i]['Reviewer ' . $j . ' Nilai'] = '0';
                 }
-                $data[$i]['Reviewer ' . $j++] = $member_info->full_name;
+                $j++;
             }
             while ($j <= 4)
             {
                 $data[$i]['Reviewer ' . $j++] = "";
             }
+            $count_reviewer = count($research_reviewers);
+            if($count_reviewer > 0)
+            {
+                $avg_score = $avg_score / $count_reviewer;
+                $avg_amount = $avg_amount / $count_reviewer;
+            }
             $data[$i]['Biaya Diusulkan'] = $propose->total_amount;
+            $data[$i]['Rata-rata Nilai'] = $avg_score;
+            $data[$i]['Rata-rata Rekomendasi Dana'] = $avg_amount;
 
             $i++;
         }
